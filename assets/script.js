63d4a756e518b7e178d8676540e8c84652df64b7
Peachplace Deisler <avadeisler@gmail.com>
2024-04-20 23:46:03 -0500

    added html and key

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,5 @@
-const APIKey = "8b49a7c3b52f6ab569b8a35dcd36322c";
 
-let city;
-
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={APIkey}
-
-const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-
-
+let apiKey = "8b49a7c3b52f6ab569b8a35dcd36322c";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=en";
 
